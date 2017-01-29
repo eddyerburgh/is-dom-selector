a496b2756864205887103dbdb75a943b19d0f7df
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-29 14:09:51 +0000

    Test that isCssSelector returns false if passed an invalid DOM selector

--- test/isCssSelector.spec.js
+++ test/isCssSelector.spec.js
@@ -1,26 +1,32 @@
 const test = require('tape');
 const isCssSelector = require('../lib/isCssSelector');
 
-test('returns true if passed a class', (t) => {
+test('isCssSelector returns true if passed a class', (t) => {
   t.plan(1);
 
   t.equal(isCssSelector('.class'), true);
 });
 
-test('returns true if passed an string with a tag', (t) => {
+test('isCssSelector returns true if passed an string with a tag', (t) => {
   t.plan(1);
 
   t.equal(isCssSelector('tag'), true);
 });
 
-test('returns true if passed an string with an id', (t) => {
+test('isCssSelector returns true if passed an string with an id', (t) => {
   t.plan(1);
 
   t.equal(isCssSelector('#id'), true);
 });
 
-test('returns true if passed an string with an attribute selector', (t) => {
+test('isCssSelector returns true if passed an string with an attribute selector', (t) => {
   t.plan(1);
 
   t.equal(isCssSelector('[attribute="value"]'), true);
+});
+
+test('isCssSelector returns false if passed string that is not valid selector', (t) => {
+  t.plan(1);
+
+  t.equal(isCssSelector('invalidselector]'), false);
 });
