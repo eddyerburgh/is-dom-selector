b0f13f957ca4a2055b33dc3733e736cc5e943fac
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-29 15:34:11 +0000

    Add test cases for input that is not a string and validate input inside function

--- test/isDomSelector.spec.js
+++ test/isDomSelector.spec.js
@@ -7,37 +7,73 @@ test('isDomSelector returns true if passed a class', (t) => {
   t.equal(isDomSelector('.class'), true);
 });
 
-test('isDomSelector returns true if passed an string with a tag', (t) => {
+test('isDomSelector returns true when passed an string with a tag', (t) => {
   t.plan(1);
 
   t.equal(isDomSelector('tag'), true);
 });
 
-test('isDomSelector returns true if passed an string with an id', (t) => {
+test('isDomSelector returns true when passed an string with an id', (t) => {
   t.plan(1);
 
   t.equal(isDomSelector('#id'), true);
 });
 
-test('isDomSelector returns true if passed an string with an attribute selector', (t) => {
+test('isDomSelector returns true when passed an string with an attribute selector', (t) => {
   t.plan(1);
 
   t.equal(isDomSelector('[attribute="value"]'), true);
 });
 
-test('isDomSelector returns true if passed a string with multiple class selectors', (t) => {
+test('isDomSelector returns true when passed a string with multiple class selectors', (t) => {
   t.plan(1);
 
   t.equal(isDomSelector('.class .another-class'), true);
 });
 
-test('isDomSelector returns true if passed a string with multiple class selectors using a combinator', (t) => {
+test('isDomSelector returns true when passed a string with multiple class selectors using a combinator', (t) => {
   t.plan(1);
 
   t.equal(isDomSelector('.class > .another-class'), true);
 });
 
-test('isDomSelector returns false if passed string that is not valid selector', (t) => {
+test('isDomSelector returns false when passed an object', (t) => {
+  t.plan(1);
+
+  t.equal(isDomSelector({}), false);
+});
+
+test('isDomSelector returns false when passed an array', (t) => {
+  t.plan(1);
+
+  t.equal(isDomSelector([]), false);
+});
+
+test('isDomSelector returns false when passed null', (t) => {
+  t.plan(1);
+
+  t.equal(isDomSelector(null), false);
+});
+
+test('isDomSelector returns false when passed a number', (t) => {
+  t.plan(1);
+
+  t.equal(isDomSelector(1), false);
+});
+
+test('isDomSelector returns false when passed NaN', (t) => {
+  t.plan(1);
+
+  t.equal(isDomSelector(NaN), false);
+});
+
+test('isDomSelector returns false when passed undefined', (t) => {
+  t.plan(1);
+
+  t.equal(isDomSelector(undefined), false);
+});
+
+test('isDomSelector returns false when passed a string that is not valid selector', (t) => {
   t.plan(1);
 
   t.equal(isDomSelector('not $$ valid selector'), false);
